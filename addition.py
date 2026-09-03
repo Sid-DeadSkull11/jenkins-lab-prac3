e27b7664893330d10ca7cbee0e7ac739ed3ec0c9
Sid_DeadSkull11 <siddhu2005blr@gmail.com>
2026-09-03 10:33:23 +0530

    addition verison 2

--- addition.py
+++ addition.py
@@ -1,7 +1,9 @@
+import sys
+
 def add(a,b):
     return (a+b)
 
 if __name__=="__main__":
-    a=int(input("Enter first number: "))
-    b=int(input("Enter second number: "))
+    a=int(sys.argv[1])
+    b=int(sys.argv[2])
     print(add(a,b))
